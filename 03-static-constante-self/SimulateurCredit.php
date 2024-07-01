4b83265f9b3d88b07f3576150ed8ac13342861e3
Edjek <edjek@hotmail.fr>
2024-07-01 16:46:40 +0200

    first day

---
+++ 03-static-constante-self/SimulateurCredit.php
@@ -0,0 +1,21 @@
+<?php
+
+class SimulateurCredit
+{
+    public const TAUX = 20.5;
+
+    /**
+     * @var float
+     */
+    private static float $quotient = 0.2;
+
+    /**
+     * @param int $pret
+     * 
+     * @return float
+     */
+    public static function calculInteret(int $pret): float
+    {
+        return $pret * self::$quotient;
+    }
+}
