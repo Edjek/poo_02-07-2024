92971e4fc11470e8c7e62ed30067de17df549ca0
Edjek <edjek@hotmail.fr>
2024-07-03 15:38:58 +0200

    third day

--- 08-agregation-composition/index.php
+++ 08-agregation-composition/index.php
@@ -1,18 +1,15 @@
 <?php
 
+require_once './Database.php';
 require_once './DatabaseAgregation.php';
 
-$conn = new Database();
-$conn->dbConnect();
-$pdo = $conn->getPdo();
+// Composition exemple
+$db = new Database();
+$db->dbConnect();
+$conn = $db->getPdo();
 
-// CREER une classe DatabaseAgregation
-    // propriété $pdo type PDO
-    // constructeur qui prend en parametre un objet PDO
-        // initialise la proporiété $pdo grace au parametre
+// Agregation exemple
+$pdo = new PDO('mysql:host=localhost;dbname=library_db', 'root', '');
 
-
-        $z = new PDO('mysql:host=localhost;dbname=library_db', 'root', '', [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-
-        $conn = new DatabaseAgregation($z);
-        $conn->getPdo();
+$dbAgregation = new DatabaseAgregation($pdo);
+$dbAgregation->getPdo();
