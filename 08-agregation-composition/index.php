747d863b5ae69c473c04f22dd195291422068b8e
Edjek <edjek@hotmail.fr>
2024-07-03 15:26:53 +0200

    third day

--- 08-agregation-composition/index.php
+++ 08-agregation-composition/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './Database.php';
+require_once './DatabaseAgregation.php';
 
 $conn = new Database();
 $conn->dbConnect();
@@ -10,3 +10,9 @@ $pdo = $conn->getPdo();
     // propriété $pdo type PDO
     // constructeur qui prend en parametre un objet PDO
         // initialise la proporiété $pdo grace au parametre
+
+
+        $z = new PDO('mysql:host=localhost;dbname=library_db', 'root', '', [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
+
+        $conn = new DatabaseAgregation($z);
+        $conn->getPdo();
