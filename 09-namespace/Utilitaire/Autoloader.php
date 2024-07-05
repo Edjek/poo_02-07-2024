3f442fc94faf8473b19deadff702dff3cf299b17
Edjek <edjek@hotmail.fr>
2024-07-05 11:44:07 +0200

    fifth day

--- 09-namespace/Utilitaire/Autoloader.php
+++ 09-namespace/Utilitaire/Autoloader.php
@@ -7,13 +7,11 @@ class Autoloader
     public static function autoload()
     {
         spl_autoload_register(function ($className) {
-
             $dirPath = str_replace(DIRECTORY_SEPARATOR.'Utilitaire', '', __DIR__);
             $className = str_replace('App', '', $className);
             $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
 
             require_once $dirPath.$className. '.php';
-            echo $dirPath.$className. '.php'. '<br>';
         });
     }
 }
